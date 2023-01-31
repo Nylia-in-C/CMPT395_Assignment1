6061d4b0f1d18822db6adea4eefd22348231ab88
Nylia-in-C <nyliadigital@gmail.com>
2023-01-31 13:32:33 -0700

    Refactor password check for security standards

--- kata3.py
+++ kata3.py
@@ -9,8 +9,23 @@
 #Return:        is_valid - Bool for password validity
 #               err_msg - string indicating why password is not valid
 
+#Note: Due to being a password function, the default fail-safe is to
+#return false unless every check passes. This results in longer code
+#but better complies with secure coding logic.
+
 def pass_val(string):
-    if (len(string) < 8):
+    if (len(string) >= 8): #string length of min 8
+        is_min8 = True
+    else:
         err_msg = "Password must be at least 8 characters"
         return (False, err_msg)
-    return (True, "")
+    
+
+    if (is_min8 == True): #check if all flags are true
+        return (True, "")
+
+    else:
+        return (False, err_msg)
+
+if __name__ == "__main__":
+    pass_val("testing1234")
